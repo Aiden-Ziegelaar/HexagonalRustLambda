a480b124069bda27b271ab820a05afaa2c503e62
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2023-09-30 11:00:02 +0800

    feat: Added user_username_update tests

--- services/users/user_username_update/domain.rs
+++ services/users/user_username_update/domain.rs
@@ -25,3 +25,108 @@ pub async fn user_username_update_core<T1: UserRepositoryPort, T2: EventingPort>
 
     result
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_user_username_update_core() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let mut eventing_port = eventing::MockEventingPort::new();
+
+        let email = "thisEmailIsNotValidated".to_string();
+        let username = "thisUsernameIsNotValidated".to_string();
+
+        user_repository_port
+            .expect_user_update_username_by_email()
+            .times(1)
+            .returning(move |_, _| Ok(()));
+
+        eventing_port
+            .expect_emit::<EventUsernameUpdatedV1>()
+            .times(1)
+            .returning(move |_| Ok(()));
+
+        // Act
+        let result = user_username_update_core(
+            &user_repository_port,
+            &eventing_port,
+            email.clone(),
+            username.clone(),
+        )
+        .await;
+
+        // Assert
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_user_username_update_core_error() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let eventing_port = eventing::MockEventingPort::new();
+
+        let email = "thisEmailIsNotValidated".to_string();
+        let username = "thisUsernameIsNotValidated".to_string();
+
+        user_repository_port
+            .expect_user_update_username_by_email()
+            .times(1)
+            .returning(move |_, _| Err(HexagonalError {
+                error: error::HexagonalErrorCode::NotFound,
+                message: "test".to_string(),
+                trace: "".to_string(),
+            }));
+
+        // Act
+        let result = user_username_update_core(
+            &user_repository_port,
+            &eventing_port,
+            email.clone(),
+            username.clone(),
+        )
+        .await;
+
+        // Assert
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_user_username_update_core_error_from_eventing() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let mut eventing_port = eventing::MockEventingPort::new();
+
+        let email = "thisEmailIsNotValidated".to_string();
+        let username = "thisUsernameIsNotValidated".to_string();
+
+        user_repository_port
+            .expect_user_update_username_by_email()
+            .times(1)
+            .returning(move |_, _| Ok(()));
+
+        eventing_port
+            .expect_emit::<EventUsernameUpdatedV1>()
+            .times(1)
+            .returning(move |_| Err(HexagonalError {
+                error: error::HexagonalErrorCode::NotFound,
+                message: "test".to_string(),
+                trace: "".to_string(),
+            }));
+
+        // Act
+        let result = user_username_update_core(
+            &user_repository_port,
+            &eventing_port,
+            email.clone(),
+            username.clone(),
+        )
+        .await;
+
+        // Assert
+        assert!(result.is_err());
+    }
+}
