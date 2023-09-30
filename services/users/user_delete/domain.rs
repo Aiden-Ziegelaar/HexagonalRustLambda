74e6da8ea040f8f5adac0be4257f1982f05a4615
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2023-09-30 10:46:39 +0800

    feat: user_delete tests

--- services/users/user_delete/domain.rs
+++ services/users/user_delete/domain.rs
@@ -19,3 +19,97 @@ pub async fn user_delete_core<T1: UserRepositoryPort, T2: EventingPort>(
 
     user
 }
+
+#[cfg(test)]
+mod tests {
+    use models::default_time;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_user_delete_core() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let mut eventing_port = eventing::MockEventingPort::new();
+
+        let email = "thisEmailIsNotValidated".to_string();
+
+        let user = User {
+            email: email.clone(),
+            first: "first".to_string(),
+            last: "last".to_string(),
+            username: "username".to_string(),
+            created_at: default_time(),
+            updated_at: default_time(),
+        };
+
+        let return_user = user.clone();
+
+        user_repository_port.expect_user_delete_by_email().times(1).returning(move |_| Ok(return_user.clone()));
+
+        eventing_port.expect_emit::<EventUserDeletedV1>().times(1).returning(move |_| Ok(()));
+
+        // Act
+        let result = user_delete_core(&user_repository_port, &eventing_port, email.clone()).await;
+
+        // Assert
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), user);        
+    }
+
+    #[tokio::test]
+    async fn test_user_delete_not_found() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let eventing_port = eventing::MockEventingPort::new();
+
+        let email = "thisEmailIsNotValidated".to_string();
+
+        user_repository_port.expect_user_delete_by_email().times(1).returning(move |_| Err(HexagonalError {
+            error: error::HexagonalErrorCode::NotFound,
+            message: "User not found".to_string(),
+            trace: "".to_string(),
+        }));
+
+        // Act
+        let result = user_delete_core(&user_repository_port, &eventing_port, email.clone()).await;
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().error, error::HexagonalErrorCode::NotFound);
+    }
+
+    #[tokio::test]
+    async fn test_user_delete_eventing_failure() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let mut eventing_port = eventing::MockEventingPort::new();
+
+        let email = "thisEmailIsNotValidated".to_string();
+
+        let user = User {
+            email: email.clone(),
+            first: "first".to_string(),
+            last: "last".to_string(),
+            username: "username".to_string(),
+            created_at: default_time(),
+            updated_at: default_time(),
+        };
+
+        user_repository_port.expect_user_delete_by_email().times(1).returning(move |_| Ok(user.clone()));
+
+        eventing_port.expect_emit::<EventUserDeletedV1>().times(1).returning(move |_| Err(HexagonalError {
+            error: error::HexagonalErrorCode::AdaptorError,
+            message: "Error in Eventing".to_string(),
+            trace: "".to_string(),
+        }));
+
+        // Act
+        let result = user_delete_core(&user_repository_port, &eventing_port, email.clone()).await;
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().error, error::HexagonalErrorCode::AdaptorError);
+    }
+}
+
