11b471a7f6ecd198dc57d4a54644d9ae72258456
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2023-09-30 12:44:09 +0800

    fix: Added username update integration tests

--- test-integration/test/user/user_username_update.mjs
+++ test-integration/test/user/user_username_update.mjs
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { assert, expect } from 'chai';
 import { faker } from '@faker-js/faker';
 
-describe('Update User', function () {
+describe('Update User Username', function () {
     it('should update a username', async function () {
         //arrange
         let user = {
@@ -39,5 +39,84 @@ describe('Update User', function () {
         //assert
         expect(res.data).to.include(patched_user)
         assert.equal(res.status, 200)
+    })
+
+    it('should allow creation of a user with a username that was previously taken after update', async function () {
+        //arrange
+        let user = {
+            first: faker.person.firstName(),
+            last: faker.person.lastName(),
+            email: faker.internet.email().toLowerCase(),
+            username: faker.internet.userName(),
+        }
+
+        let user_after_update = {
+            first: faker.person.firstName(),
+            last: faker.person.lastName(),
+            email: faker.internet.email().toLowerCase(),
+            username: user.username,
+        }
+
+        let username_patch = {
+            email: user.email,
+            username: faker.internet.userName(),
+        }
+
+        //act
+        await axios.post(`${process.env.INF_API_ENDPOINT}main/user`, user)
+
+        await axios.put(`${process.env.INF_API_ENDPOINT}main/user/username`, 
+            username_patch,
+            {
+                validateStatus: () => true,
+            }
+        )
+
+        let res = await axios.post(`${process.env.INF_API_ENDPOINT}main/user`, user_after_update)
+
+        //assert
+        expect(res.data).to.include(user_after_update)
+        assert.equal(res.status, 201)
+    })
+
+    it('should not allow creation of a user with a username that was updated to', async function () {
+        //arrange
+        let user = {
+            first: faker.person.firstName(),
+            last: faker.person.lastName(),
+            email: faker.internet.email().toLowerCase(),
+            username: faker.internet.userName(),
+        }
+
+        let username_patch = {
+            email: user.email,
+            username: faker.internet.userName(),
+        }
+
+        let user_after_update = {
+            first: faker.person.firstName(),
+            last: faker.person.lastName(),
+            email: faker.internet.email().toLowerCase(),
+            username: username_patch.username,
+        }
+
+        //act
+        await axios.post(`${process.env.INF_API_ENDPOINT}main/user`, user)
+
+        await axios.put(`${process.env.INF_API_ENDPOINT}main/user/username`, 
+            username_patch,
+            {
+                validateStatus: () => true,
+            }
+        )
+
+        let res = await axios.post(`${process.env.INF_API_ENDPOINT}main/user`, user_after_update,
+            {
+                validateStatus: () => true,
+            }
+        )
+
+        //assert
+        assert.equal(res.status, 409)
     })
 });
