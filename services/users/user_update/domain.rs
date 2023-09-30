c4ceef4dc5645711805dbe6fa82d9b67539932ce
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2023-09-30 10:57:14 +0800

    feat: user_update tests

--- services/users/user_update/domain.rs
+++ services/users/user_update/domain.rs
@@ -27,3 +27,146 @@ pub async fn user_update_core<T1: UserRepositoryPort, T2: EventingPort>(
 
     user
 }
+
+#[cfg(test)]
+mod tests {
+    use models::default_time;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_user_update_core() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let mut eventing_port = eventing::MockEventingPort::new();
+
+        let user = User {
+            email: "testemail".to_string(),
+            first: "first".to_string(),
+            last: "last".to_string(),
+            username: "username".to_string(),
+            created_at: default_time(),
+            updated_at: default_time(),
+        };
+
+        let mutable_user = MutableUser {
+            email: user.email.clone(),
+            first: Some("first".to_string()),
+            last: Some("last".to_string()),
+        };
+
+        let return_user = user.clone();
+
+        user_repository_port.expect_user_update_by_email().times(1).returning(move |_| Ok(return_user.clone()));
+
+        eventing_port.expect_emit::<EventUserUpdatedV1>().times(1).returning(move |_| Ok(()));
+
+        // Act
+        let result = user_update_core(&user_repository_port, &eventing_port, mutable_user.clone()).await;
+
+        // Assert
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), user);
+    }
+
+    #[tokio::test]
+    async fn test_user_update_core_no_updates() {
+        // Arrange
+        let user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let eventing_port = eventing::MockEventingPort::new();
+
+        let user = User {
+            email: "testemail".to_string(),
+            first: "first".to_string(),
+            last: "last".to_string(),
+            username: "username".to_string(),
+            created_at: default_time(),
+            updated_at: default_time(),
+        };
+
+        let mutable_user = MutableUser {
+            email: user.email.clone(),
+            first: None,
+            last: None,
+        };
+
+        // Act
+        let result = user_update_core(&user_repository_port, &eventing_port, mutable_user.clone()).await;
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().error, error::HexagonalErrorCode::BadInput);
+    }
+
+    #[tokio::test]
+    async fn test_user_update_core_error_from_dynamo() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let eventing_port = eventing::MockEventingPort::new();
+
+        let user = User {
+            email: "testemail".to_string(),
+            first: "first".to_string(),
+            last: "last".to_string(),
+            username: "username".to_string(),
+            created_at: default_time(),
+            updated_at: default_time(),
+        };
+
+        let mutable_user = MutableUser {
+            email: user.email.clone(),
+            first: Some("first".to_string()),
+            last: Some("last".to_string()),
+        };
+
+        user_repository_port.expect_user_update_by_email().times(1).returning(move |_| Err(HexagonalError {
+            error: error::HexagonalErrorCode::AdaptorError,
+            message: "test".to_string(),
+            trace: "".to_string(),
+        }));
+
+        // Act
+        let result = user_update_core(&user_repository_port, &eventing_port, mutable_user.clone()).await;
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().error, error::HexagonalErrorCode::AdaptorError);
+    }
+
+    #[tokio::test]
+    async fn test_user_update_core_error_from_eventing() {
+        // Arrange
+        let mut user_repository_port = models::models::user::MockUserRepositoryPort::new();
+        let mut eventing_port = eventing::MockEventingPort::new();
+
+        let user = User {
+            email: "testemail".to_string(),
+            first: "first".to_string(),
+            last: "last".to_string(),
+            username: "username".to_string(),
+            created_at: default_time(),
+            updated_at: default_time(),
+        };
+
+        let mutable_user = MutableUser {
+            email: user.email.clone(),
+            first: Some("first".to_string()),
+            last: Some("last".to_string()),
+        };
+
+        user_repository_port.expect_user_update_by_email().times(1).returning(move |_| Ok(user.clone()));
+
+        eventing_port.expect_emit::<EventUserUpdatedV1>().times(1).returning(move |_| Err(HexagonalError {
+            error: error::HexagonalErrorCode::AdaptorError,
+            message: "test".to_string(),
+            trace: "".to_string(),
+        }));
+
+        // Act
+        let result = user_update_core(&user_repository_port, &eventing_port, mutable_user.clone()).await;
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().error, error::HexagonalErrorCode::AdaptorError);
+    }
+}
